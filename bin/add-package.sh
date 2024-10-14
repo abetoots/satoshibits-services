2db5896849cf1c8fd4b33b54fb36e44c20261b6f
Abe Caymo <caymo.abesuni@gmail.com>
2024-10-14 14:55:21 +0800

    fix(bin): already handled by vitest by default

--- bin/add-package.sh
+++ bin/add-package.sh
@@ -169,11 +169,7 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
-    globals: true,
-    include: [
-      "**/__tests__/**/*.(c|m)?[jt]s",
-      "**/?(*.)+(spec|test).(c|m)?[jt]s",
-    ],
+    globals: true
   },
 });
 
