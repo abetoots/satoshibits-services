74b8e0c41f63ee68e57be740439f3e5a5318d5fd
Abe Caymo <caymo.abesuni@gmail.com>
2025-03-18 19:58:29 +0800

    refactor(bin): update react template

--- bin/react-template/tailwind.config.ts
+++
@@ -1,8 +0,0 @@
-import formsPlugin from "@tailwindcss/forms";
-
-import type { Config } from "tailwindcss";
-
-export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  plugins: [formsPlugin()],
-} satisfies Config;
